b4c28ee4f0094469e0a8b8400f04ed095b2ddc20
Adam Ballesteros <adam@midwestpanel.com>
2025-06-19 10:34:41 -0500

    Add homepage

--- pages/index.tsx
+++
@@ -1,8 +0,0 @@
-export default function Home() {
-  return (
-    <div className="p-6 text-center text-blue-900">
-      <h1 className="text-3xl font-bold mb-4">MWP Packing List</h1>
-      <p className="text-lg">Welcome! App is ready for deployment and iPad installation.</p>
-    </div>
-  );
-}
